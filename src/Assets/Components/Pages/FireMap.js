e8976143fb0f80f311cc551d0721d1cc90da7441
JustAG7 <vophong2205@gmail.com>
2023-06-23 18:06:12 +0700

    update map

--- src/Assets/Components/Pages/FireMap.js
+++ src/Assets/Components/Pages/FireMap.js
@@ -1,12 +1,16 @@
 import {memo} from 'react'
 import '../../CSS/AboutUs.css';
 import CarouselSwiper from '../LittleComponent/CarouselSwiper';
+import Firemap from '../LittleComponent/FireMap';
+import Highcharts from 'highcharts';
+
 
 function FireMap() {
     return (
         
         <div>
-
+            
+            <Firemap/>
         </div>
     );
 }
